c3b80c7c8a8ffa076c75bb2b2af33b4706a769fe
AnthonySinitsa <itonyemail@gmail.com>
2024-06-26 20:56:58 +0000

    updated shadermanagerclass to have terrain shader

--- Terrain/shadermanagerclass.h
+++ Terrain/shadermanagerclass.h
@@ -10,6 +10,7 @@
 #include "textureshaderclass.h"
 #include "lightshaderclass.h"
 #include "fontshaderclass.h"
+#include "terrainshaderclass.h"
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -29,12 +30,14 @@ public:
 	bool RenderTextureShader(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*);
 	bool RenderLightShader(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, XMFLOAT3, XMFLOAT4);
 	bool RenderFontShader(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, XMFLOAT4);
+	bool RenderTerrainShader(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, XMFLOAT3, XMFLOAT4);
 
 private:
 	ColorShaderClass* m_ColorShader;
 	TextureShaderClass* m_TextureShader;
 	LightShaderClass* m_LightShader;
 	FontShaderClass* m_FontShader;
+	TerrainShaderClass* m_TerrainShader;
 };
 
 #endif
